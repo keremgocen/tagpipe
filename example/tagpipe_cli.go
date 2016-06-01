d9412182e87b36d7bd88de33f0492fda9ffcf3bc
kerem <keremgocen@gmail.com>
2016-06-02 00:45:03 +0300

    added pipeline for file io
    - nested directory parsing
    - tag counting is now distributed over a limited digesters, results are
    channeled back through the md5
    - missing md5 cache

--- example/tagpipe_cli.go
+++ example/tagpipe_cli.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/keremgocen/tagpipe"
 )
@@ -11,14 +13,28 @@ import (
 const dataPath = "../data/"
 
 func main() {
-	// var tags []string
+	var tags []string
 	// tagpipe.Cache = make(map[string]tagpipe.FileCache)
 	// tM := make(map[string]int) // tag map keeping total counts
 	// files, _ := ioutil.ReadDir(dataPath)
 
+	// determine tags to be parsed - this part is blocking
+	if len(os.Args[1:]) > 0 { // os.Args[1:] holds the arguments to the program.
+		tags = os.Args[1:]
+	} else { // parse arguments from 'tags.txt'
+		fmt.Println("Missing command-line arguments! Fetching from the file `tags.txt`..")
+		dat, err := ioutil.ReadFile("../tags.txt")
+		if err != nil {
+			fmt.Println("ReadFile returned error:", err)
+		}
+		tags = strings.Fields(string(dat))
+	}
+
+	fmt.Println(tags)
+
 	// Calculate the MD5 sum of all files under the specified directory,
 	// then print the results sorted by path name.
-	m, err := tagpipe.MD5All(dataPath)
+	m, err := tagpipe.MD5All(dataPath, tags)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -27,55 +43,11 @@ func main() {
 	for path := range m {
 		paths = append(paths, path)
 	}
-	sort.Strings(paths)
-	for _, path := range paths {
-		fmt.Printf("%x  %s\n", m[path], path)
-	}
-	//
-	// if len(os.Args[1:]) > 0 { // os.Args[1:] holds the arguments to the program.
-	// 	tags = os.Args[1:]
-	// } else { // parse arguments from 'tags.txt'
-	// 	fmt.Println("Missing command-line arguments! Fetching from the file `tags.txt`..")
-	// 	dat, err := ioutil.ReadFile("../tags.txt")
-	// 	tagpipe.Check(err)
-	// 	tags = strings.Fields(string(dat))
+	// sort.Strings(paths)
+	// for _, path := range paths {
+	// 	fmt.Printf("%x  %s\n", m[path], path)
 	// }
-	//
-	// for _, f := range files { // search for tags in each file asynchronously
-	//
-	// 	// lookup for file in cache, avoid parsing again
-	// 	// fmt.Println("--metadata name:", f.Name(), " mod_time:", f.ModTime(), " size:", f.Size())
-	// 	md5Str := f.Name() + f.ModTime().String() + string(f.Size())
-	// 	hash := md5.New()
-	// 	io.WriteString(hash, md5Str)
-	// 	fmt.Printf("\nfile:%s, md5:%x\n", f.Name(), hash.Sum(nil))
-	//
-	// 	content, err := ioutil.ReadFile(dataPath + f.Name())
-	// 	tagpipe.Check(err)
-	//
-	// 	fileHasValidJSON := tagpipe.IsValidJSON(string(content))
-	//
-	// 	if fileHasValidJSON {
-	// 		// fmt.Printf("contents:%s,  \nisJSON:%v\n---------------------\n", string(content), true)
-	// 		for _, tag := range tags {
-	// 			r, _ := regexp.Compile(tag)
-	// 			c := len(r.FindAllString(string(content), -1))
-	// 			if c > 0 {
-	// 				tM[tag]++
-	// 			}
-	// 			fmt.Println("tag:", tag, " - count:", c)
-	// 			// if r.Match(content) {
-	// 			// 	fmt.Println("found tag:", tag, " in file:", f.Name(), " count:")
-	// 			// }
-	// 		}
-	// 		fmt.Printf("---------------------\n")
-	// 	} else {
-	// 		fmt.Println("skipping file ", f.Name(), " with invalid JSON")
-	// 		fmt.Println("---------------------")
-	// 	}
-	//
-	// }
-	//
+
 	// sT := make(tagpipe.SortedTagCounts, len(tM))
 	// i := 0
 	// for k, v := range tM {
--- tagpipe.go
+++ tagpipe.go
@@ -1,7 +1,6 @@
 package tagpipe
 
 import (
-	"bufio"
 	"crypto/md5"
 	"encoding/json"
 	"errors"
@@ -10,7 +9,6 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"strings"
 	"sync"
 	"time"
 )
@@ -20,6 +18,7 @@ type result struct {
 	path string
 	sum  [md5.Size]byte
 	err  error
+	tmap *map[string]int
 }
 
 // FileCache holds info of each file, including tags observed per file
@@ -45,152 +44,204 @@ func (p SortedTagCounts) Len() int           { return len(p) }
 func (p SortedTagCounts) Less(i, j int) bool { return p[i].Value < p[j].Value }
 func (p SortedTagCounts) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-// Check exits on error
-func Check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-// sumFiles starts goroutines to walk the directory tree at root and digest each
-// regular file.  These goroutines send the results of the digests on the result
-// channel and send the result of the walk on the error channel.  If done is
-// closed, sumFiles abandons its work.
-func sumFiles(done <-chan struct{}, root string) (<-chan result, <-chan error) {
-	// For each regular file, start a goroutine that sums the file and sends
-	// the result on c.  Send the result of the walk on errc.
-	c := make(chan result)
+// walkFiles starts a goroutine to walk the directory tree at root and send the
+// path of each regular file on the string channel.  It sends the result of the
+// walk on the error channel.  If done is closed, walkFiles abandons its work.
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
 	errc := make(chan error, 1)
 	go func() { // HL
-		var wg sync.WaitGroup
-		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		// Close the paths channel after Walk returns.
+		defer close(paths) // HL
+		// No select needed for this send, since errc is buffered.
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error { // HL
 			if err != nil {
 				return err
 			}
 			if !info.Mode().IsRegular() {
 				return nil
 			}
-			wg.Add(1)
-			go func() { // HL
-				data, err := ioutil.ReadFile(path)
-				select {
-				case c <- result{path, md5.Sum(data), err}: // HL
-				case <-done: // HL
-				}
-				wg.Done()
-			}()
-			// Abort the walk if done is closed.
 			select {
+			case paths <- path: // HL
 			case <-done: // HL
 				return errors.New("walk canceled")
-			default:
-				return nil
 			}
+			return nil
 		})
-		// Walk has returned, so all calls to wg.Add are done.  Start a
-		// goroutine to close c once all the sends are done.
-		go func() { // HL
-			wg.Wait()
-			close(c) // HL
-		}()
-		// No select needed here, since errc is buffered.
-		errc <- err // HL
 	}()
-	return c, errc
+	return paths, errc
+}
+
+// digester reads path names from paths and sends digests of the corresponding
+// files on c until either paths or done is closed.
+func digester(done <-chan struct{}, paths <-chan string, c chan<- result, tags []string) {
+
+	for path := range paths { // HLpaths
+		data, err := ioutil.ReadFile(path)
+
+		// discard files containing invalid JSON
+		fileHasValidJSON := IsValidJSON(string(data))
+
+		tM := make(map[string]int) // tag map keeping total counts
+
+		if fileHasValidJSON {
+
+			// debug
+			var y map[string]interface{}
+			json.Unmarshal(data, &y)
+			fmt.Println("\npath:", path, " content:", y)
+
+			// fmt.Printf("contents:%s,  \nisJSON:%v\n---------------------\n", string(content), true)
+			for _, tag := range tags {
+				r, _ := regexp.Compile("\"" + tag + "\"")
+				c := len(r.FindAllStringIndex(string(data), -1))
+				if c > 0 {
+					tM[tag] += c
+				}
+				fmt.Println("tag:", tag, " - count:", c)
+				// if r.Match(data) {
+				// 	fmt.Println("found tag:", tag, " in file:", path, " count:", len(r.FindAllStringSubmatchIndex(string(data), -1)))
+				// }
+			}
+
+		} else {
+			fmt.Println("\nskipping file ", path, " with invalid JSON")
+			return
+		}
+
+		// debug
+		if len(tM) > 0 {
+			fmt.Println("\nfound tags:", tM, " in file:", path)
+		}
+
+		select {
+		case <-done:
+			return
+		default:
+			c <- result{path, md5.Sum(data), err, &tM}
+		}
+	}
 }
 
 // MD5All reads all the files in the file tree rooted at root and returns a map
 // from file path to the MD5 sum of the file's contents.  If the directory walk
 // fails or any read operation fails, MD5All returns an error.  In that case,
 // MD5All does not wait for inflight read operations to complete.
-func MD5All(root string) (map[string][md5.Size]byte, error) {
+func MD5All(root string, tags []string) (map[string][md5.Size]byte, error) {
 	// MD5All closes the done channel when it returns; it may do so before
 	// receiving all the values from c and errc.
-	done := make(chan struct{}) // HLdone
-	defer close(done)           // HLdone
+	done := make(chan struct{})
+	defer close(done)
 
-	c, errc := sumFiles(done, root) // HLdone
+	paths, errc := walkFiles(done, root)
+
+	totaltags := make(map[string]int) // tag map keeping total counts
+
+	// Start a fixed number of goroutines to read and digest files.
+	c := make(chan result) // HLc
+	var wg sync.WaitGroup
+	const numDigesters = 20
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, paths, c, tags) // HLc
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c) // HLc
+	}()
+	// End of pipeline. OMIT
 
 	m := make(map[string][md5.Size]byte)
-	for r := range c { // HLrange
+	for r := range c {
 		if r.err != nil {
 			return nil, r.err
 		}
 		m[r.path] = r.sum
+		fmt.Println("received tags:")
+		for t, i := range *r.tmap {
+			fmt.Println(t, i)
+			totaltags[t] += i
+		}
 	}
-	if err := <-errc; err != nil {
+
+	fmt.Println(totaltags)
+	// Check whether the Walk failed.
+	if err := <-errc; err != nil { // HLerrc
 		return nil, err
 	}
 	return m, nil
 }
 
-// CountTagsInFile counts all given tags inside the file
-func CountTagsInFile(file *strings.Reader, tag string) int {
-
-	var telephone = regexp.MustCompile(`[A-Za-z]+`)
-	// var telephone = regexp.MustCompile(`\(\d+\)\s\d+-\d+`)
-
-	// do I need buffered channels here?
-	tags := make(chan string)
-	results := make(chan int)
-
-	// I think we need a wait group, not sure.
-	wg := new(sync.WaitGroup)
-
-	// start up some workers that will block and wait?
-	for w := 1; w <= 3; w++ {
-		wg.Add(1)
-		go MatchTags(tags, results, wg, telephone)
-	}
-
-	// Go over a file line by line and queue up a ton of work
-	go func() {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			// Later I want to create a buffer of lines, not just line-by-line here ...
-			tags <- scanner.Text()
-		}
-		close(tags)
-	}()
-
-	func() {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			// Later I want to create a buffer of lines, not just line-by-line here ...
-			tags <- scanner.Text()
-		}
-		close(tags)
-	}()
-
-	// Now collect all the results...
-	// But first, make sure we close the result channel when everything was processed
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	// Add up the results from the results channel.
-	counts := 0
-	for v := range results {
-		counts += v
-	}
-
-	return counts
-}
-
-// MatchTags counts tags in the given file
-func MatchTags(tags <-chan string, results chan<- int, wg *sync.WaitGroup, telephone *regexp.Regexp) {
-	// func matchTags(tags <-chan string, results chan<- int, wg *sync.WaitGroup, telephone *regexp.Regexp) {
-	// Decreasing internal counter for wait-group as soon as goroutine finishes
-	defer wg.Done()
-
-	// eventually I want to have a []string channel to work on a chunk of lines not just one line of text
-	for j := range tags {
-		if telephone.MatchString(j) {
-			results <- 1
-		}
-	}
-}
+// // CountTagsInFile counts all given tags inside the file
+// func CountTagsInFile(file *strings.Reader, tag string) int {
+//
+// 	var telephone = regexp.MustCompile(`[A-Za-z]+`)
+// 	// var telephone = regexp.MustCompile(`\(\d+\)\s\d+-\d+`)
+//
+// 	// do I need buffered channels here?
+// 	tags := make(chan string)
+// 	results := make(chan int)
+//
+// 	// I think we need a wait group, not sure.
+// 	wg := new(sync.WaitGroup)
+//
+// 	// start up some workers that will block and wait?
+// 	for w := 1; w <= 3; w++ {
+// 		wg.Add(1)
+// 		go MatchTags(tags, results, wg, telephone)
+// 	}
+//
+// 	// Go over a file line by line and queue up a ton of work
+// 	go func() {
+// 		scanner := bufio.NewScanner(file)
+// 		for scanner.Scan() {
+// 			// Later I want to create a buffer of lines, not just line-by-line here ...
+// 			tags <- scanner.Text()
+// 		}
+// 		close(tags)
+// 	}()
+//
+// 	func() {
+// 		scanner := bufio.NewScanner(file)
+// 		for scanner.Scan() {
+// 			// Later I want to create a buffer of lines, not just line-by-line here ...
+// 			tags <- scanner.Text()
+// 		}
+// 		close(tags)
+// 	}()
+//
+// 	// Now collect all the results...
+// 	// But first, make sure we close the result channel when everything was processed
+// 	go func() {
+// 		wg.Wait()
+// 		close(results)
+// 	}()
+//
+// 	// Add up the results from the results channel.
+// 	counts := 0
+// 	for v := range results {
+// 		counts += v
+// 	}
+//
+// 	return counts
+// }
+//
+// // MatchTags counts tags in the given file
+// func MatchTags(tags <-chan string, results chan<- int, wg *sync.WaitGroup, telephone *regexp.Regexp) {
+// 	// func matchTags(tags <-chan string, results chan<- int, wg *sync.WaitGroup, telephone *regexp.Regexp) {
+// 	// Decreasing internal counter for wait-group as soon as goroutine finishes
+// 	defer wg.Done()
+//
+// 	// eventually I want to have a []string channel to work on a chunk of lines not just one line of text
+// 	for j := range tags {
+// 		if telephone.MatchString(j) {
+// 			results <- 1
+// 		}
+// 	}
+// }
 
 // IsValidJSON checks if the given string has a valid JSON format, generalized
 func IsValidJSON(s string) bool {
